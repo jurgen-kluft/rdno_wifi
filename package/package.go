d0c530362a9521004f5bf4aa7981dc24520a6049
jurgen.kluft <jurgen.kluft@gmail.com>
2025-10-01 19:00:53 +0800

    * rdno_wifi
    + added esp8266 wifi library

--- package/package.go
+++ package/package.go
@@ -23,15 +23,23 @@ func GetPackage() *denv.Package {
 
 	// esp32 library
 	esp32wifilib := denv.SetupCppLibProjectForArduinoEsp32(mainpkg, name+"_arduino")
-	esp32wifilib.AddEnvironmentVariable("ESP_SDK")
-	esp32wifilib.AddInclude("{ESP_SDK}", "libraries/WiFi", "src")
-	esp32wifilib.SourceFilesFrom("{ESP_SDK}", "libraries/WiFi", "src")
+	esp32wifilib.AddEnvironmentVariable("ESP32_SDK")
+	esp32wifilib.AddInclude("{ESP32_SDK}", "libraries/WiFi", "src")
+	esp32wifilib.SourceFilesFrom("{ESP32_SDK}", "libraries/WiFi", "src")
 	esp32wifilib.AddDependencies(networkpkg.GetMainLib())
+
+	// esp8266 library
+	esp8266wifilib := denv.SetupCppLibProjectForArduinoEsp8266(mainpkg, name+"_arduino")
+	esp8266wifilib.AddEnvironmentVariable("ESP8266_SDK")
+	esp8266wifilib.AddInclude("{ESP8266_SDK}", "libraries/ESP2866WiFi", "src")
+	esp8266wifilib.SourceFilesFrom("{ESP8266_SDK}", "libraries/ESP2866WiFi", "src")
+	esp8266wifilib.AddDependencies(networkpkg.GetMainLib())
 
 	// main library
 	mainlib := denv.SetupCppLibProject(mainpkg, name)
 	mainlib.AddDependencies(networkpkg.GetMainLib())
 	mainlib.AddDependency(esp32wifilib)
+	mainlib.AddDependency(esp8266wifilib)
 
 	// test library
 	testlib := denv.SetupCppTestLibProject(mainpkg, name)
@@ -39,6 +47,7 @@ func GetPackage() *denv.Package {
 
 	mainpkg.AddMainLib(mainlib)
 	mainpkg.AddMainLib(esp32wifilib)
+	mainpkg.AddMainLib(esp8266wifilib)
 	mainpkg.AddTestLib(testlib)
 	return mainpkg
 }
